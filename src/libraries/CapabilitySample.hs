554c962962f0eb2d3ca0d5db308941df2cc9d4f3
Tim Spence <timothywspence@gmail.com>
2020-11-06 09:03:43 +0000

    Add reader to capability sample

--- src/libraries/CapabilitySample.hs
+++ src/libraries/CapabilitySample.hs
@@ -24,23 +24,28 @@ newtype App a = App {runApp :: ReaderT State IO a}
   deriving
     (HasSink "log" [String], HasWriter "log" [String])
     via WriterLog (ReaderIORef (Field "log" () (MonadReader (ReaderT State IO))))
+  deriving
+    (HasSource "config" String, HasReader "config" String)
+    via (Field "config" () (MonadReader (ReaderT State IO)))
 
 data State = State
   { counter :: IORef Int,
-    log :: IORef [String]
+    log :: IORef [String],
+    config :: String
   }
   deriving (Generic)
 
-prog :: (HasState "counter" Int m, HasWriter "log" [String] m) => m (Int, [String])
+prog :: (HasReader "config" String m, HasState "counter" Int m, HasWriter "log" [String] m) => m (Int, [String])
 prog = listen @"log" $ do
   tell @"log" ["first"]
   modify @"counter" (+ 1)
   modify @"counter" (+ 1)
   tell @"log" ["second"]
+  ask @"config" >>= (tell @"log" . pure)
   get @"counter"
 
 run :: IO (Int, [String])
 run = do
   c <- newIORef 0
   l <- newIORef []
-  flip runReaderT (State c l) . runApp $ prog
+  flip runReaderT (State c l "conf") . runApp $ prog
